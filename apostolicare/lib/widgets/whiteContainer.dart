33387edf162b90b9319f3905330fee2c8f134f29
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-05-06 11:20:51 -0300

    altera sombra

--- apostolicare/lib/widgets/whiteContainer.dart
+++ apostolicare/lib/widgets/whiteContainer.dart
@@ -10,16 +10,16 @@ class WhiteContainers extends StatelessWidget
   Widget build (BuildContext context)
   {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical:10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical:5),
       decoration: BoxDecoration(
                     color: Colors.white,
                     boxShadow: [BoxShadow(
                       color: Colors.grey[900],
                       blurRadius: 10.0, // has the effect of softening the shadow
-                      spreadRadius: .5, // has the effect of extending the shadow
+                      spreadRadius: .1, // has the effect of extending the shadow
                       offset: Offset(
-                        3.0, // horizontal, move right 10
-                        3.0, // vertical, move down 10
+                        2.0, // horizontal, move right 10
+                        2.0, // vertical, move down 10
                       ))],
                     border: Border.all(
                             color: Colors.white,
